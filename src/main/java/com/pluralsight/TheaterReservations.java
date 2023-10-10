5d859ec69ceb562b534b58d30df9d4a5e443a823
Andi <alexandra.e.burke@gmail.com>
2023-10-10 15:57:44 -0400

    TheaterResPush2

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,5 +1,5 @@
 package com.pluralsight;
-import java.time.LocalDate;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 public class TheaterReservations {
@@ -7,12 +7,18 @@ public class TheaterReservations {
         Scanner theaterReservation = new Scanner(System.in);
         DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/DD/YYYY");
 
+
         System.out.println("Enter your name (FirstName LastName)");
         String nameInput = theaterReservation.nextLine();
         System.out.println("What date will you be attending (MM/DD/YYYY)");
         String dateinput = theaterReservation.nextLine();
         System.out.println("How many tickets will you need? ");
         int ticketNumberInput = theaterReservation.nextInt();
+        String pluralletter = "";
+        if ( ticketNumberInput > 1) {
+        }
+        System.out.println(ticketNumberInput + "ticket reserved for "+ (dateinput) + "under" + (nameInput));
+
 
 
 
